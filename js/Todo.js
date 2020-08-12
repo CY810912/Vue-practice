c8bd27d8f7f8f2fd767508c3f12e3e6876bdce95
eudora-hsj <eudora.hsj@gmail.com>
2020-08-12 16:45:35 +0800

    [Todo] add todo

---
+++ js/Todo.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        newTodo:'',
+        todos: [
+            {
+                id: '1',
+                title: 'hello',
+                completed: false
+            }
+        ]
+    },
+    methods: {
+        addTodo: function(){
+            var value = this.newTodo
+            var timestamp = Math.floor(Date.now())
+            if ( !value ) {
+                return
+            }
+            this.todos.push(
+                {
+                    id: timestamp,
+                    title: value,
+                    computed: false
+                }
+            ),
+            this.newTodo = ''
+        }
+    },
+    computed: {
+        filteredTodos: function(){
+
+        }
+    }
+})
