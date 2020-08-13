1592e98f81685f4f55b23188046ec77964a24539
eudora-hsj <eudora.hsj@gmail.com>
2020-08-13 15:27:35 +0800

    basic

--- js/Todo.js
+++ js/Todo.js
@@ -8,15 +8,16 @@ var app = new Vue({
                 title: 'hello',
                 completed: false
             }
-        ]
+        ],
+        visibility: 'all',
+        cacheTodo: {},
+        cacheTitle: ''
     },
     methods: {
         addTodo: function(){
             var value = this.newTodo
             var timestamp = Math.floor(Date.now())
-            if ( !value ) {
-                return
-            }
+            if ( !value ) { return }
             this.todos.push(
                 {
                     id: timestamp,
@@ -27,24 +28,44 @@ var app = new Vue({
             this.newTodo = ''
         },
         removeTodo: function(todo){
-            var newIndex = this.todos.findIndex(function(item, key){
+            var vm = this
+            var newIndex = vm.todos.findIndex(function(item, key){
                 return todo.id === item.id
             })
-            this.todos.splice(newIndex, 1)
-        },
-        editTodo: function(){
-
+            this.todos.splice(newIndex,1)
+          },
+        editTodo: function(item){
+            this.cacheTodo = item
+            this.cacheTitle = item.title
         },
         cancelEdit: function(){
-
+            this.cacheTodo = {}
         },
-        doneEdit: function(){
-
+        doneEdit: function(item){
+            item.title = this.cacheTitle
+            this.cacheTitle = ''
+            this.cacheTodo = {}
         },
     },
     computed: {
         filteredTodos: function(){
-
+            var nowTab = this.visibility
+            var showList = []
+            switch (nowTab) {
+                case 'all':
+                    showList = this.todos
+                    break
+                case 'active':
+                    this.todos.forEach(function(item){
+                        if ( !item.completed ) { showList.push(item) }
+                    })
+                    break
+                case 'completed':
+                    this.todos.forEach(function(item){
+                        if ( item.completed ) { showList.push(item) }
+                    })
+            }
+            return showList
         }
     }
 })
